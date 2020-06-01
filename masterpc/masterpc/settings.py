b03e1902f3fdb56d390cf25d372faefce86816cc
Samir Mejia <samir04m@gmail.com>
2020-06-01 13:12:24 -0500

    Mejoras en vista responsive para moviles

--- masterpc/masterpc/settings.py
+++ masterpc/masterpc/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = '5kac!^nifdtga!cc(dlc!qm7^brd9xq@s&h@g5p1w#(nd3f_ok'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
